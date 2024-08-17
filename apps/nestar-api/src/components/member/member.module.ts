490a17e1337fdd665a00be1c9314fbe7f7bec699
aris1377 <bek77519101@gmail.com>
2024-08-17 17:33:50 +0900

    feat: created member schema model

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,8 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
-  providers: [MemberResolver, MemberService]
+	//MemberSchema - bu object
+	//Schema - class
+	//'Member'-schema modeli xosil bolyapti
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
+	providers: [MemberResolver, MemberService],
 })
 export class MemberModule {}
