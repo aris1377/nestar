b29e28e2cab58bcfd2030f8ef369dfacdc214bc5
aris1377 <bek77519101@gmail.com>
2024-08-20 23:05:43 +0900

    feat: integrate validatiion pipes logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,31 +1,36 @@
-import { Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Resolver()
 export class MemberResolver {
-    //memberService - objeckt
-    //MemberService - class
-    constructor(private readonly memberService: MemberService) { }
+	//memberService - objeckt
+	//MemberService - class
+	constructor(private readonly memberService: MemberService) {}
 
-    @Mutation(() => String)
-    public async signup(): Promise<string> {
-        console.log('Mutation: sigup');
-        return this.memberService.signup();
-    }
-    @Mutation(() => String)
-    public async login(): Promise<string> {
+	@Mutation(() => String)
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<string> {
+		console.log('Mutation: sigup');
+		console.log('Input:', input);
+		return this.memberService.signup();
+	}
+	@Mutation(() => String)
+	@UsePipes(ValidationPipe)
+	public async login(@Args('input') input: LoginInput): Promise<string> {
         console.log('Mutation: login');
-        return this.memberService.login();
-    }
-    @Mutation(() => String)
-    public async updateMember(): Promise<string> {
-        console.log('Mutation: updateMember');
-        return this.memberService.updateMember();
-    }
-    @Query(() => String)
-    public async getMember(): Promise<string> {
-        console.log('Query: getMember');
-        return this.memberService.getMember();
-    }
+        console.log('Input:', input);
+		return this.memberService.login();
+	}
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation: updateMember');
+		return this.memberService.updateMember();
+	}
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Query: getMember');
+		return this.memberService.getMember();
+	}
 }
