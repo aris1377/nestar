01458ed010e03c4c349086423b7023a7766c288c
aris1377 <bek77519101@gmail.com>
2024-08-21 00:17:16 +0900

    feat: ZL-TASK

--- train.ts
+++ train.ts
@@ -1,23 +1,37 @@
+// ZL-TASK:
+
+function stringToKebab(str: string): string {
+	return str
+		.replace(/([a-z])([A-Z])/g, '$1-$2')
+		.replace(/\s+/g, '-')
+		.toLowerCase();
+}
+
+console.log(stringToKebab('I love Kebab'));
+
+// Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin.
+// Bosh harflarni kichik harflarga ham otkazsin.
+// MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
 // ZK-TASK:
 
 // Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin
 // va 5 soniyadan keyin ishini toxtatsin.
 // MASALAN: printNumbers()
 
-function printNumbers() {
-	let number = 1;
+// function printNumbers() {
+// 	let number = 1;
 
-	const intervalId = setInterval(() => {
-		console.log(number);
-		number++;
+// 	const intervalId = setInterval(() => {
+// 		console.log(number);
+// 		number++;
 
-		if (number > 5) {
-			clearInterval(intervalId);
-		}
-	}, 1000);
-}
+// 		if (number > 5) {
+// 			clearInterval(intervalId);
+// 		}
+// 	}, 1000);
+// }
 
-printNumbers();
+// printNumbers();
 
 // ZJ-TASK:
 
