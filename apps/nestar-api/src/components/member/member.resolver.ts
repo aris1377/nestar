5742cd336d64a5d7b9f685911b7a872be0483a60
aris1377 <bek77519101@gmail.com>
2024-08-20 23:54:45 +0900

    feat: develop  signup busines logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,8 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
@@ -9,19 +10,29 @@ export class MemberResolver {
 	//MemberService - class
 	constructor(private readonly memberService: MemberService) {}
 
-	@Mutation(() => String)
+	@Mutation(() => Member)
 	@UsePipes(ValidationPipe)
-	public async signup(@Args('input') input: MemberInput): Promise<string> {
-		console.log('Mutation: sigup');
-		console.log('Input:', input);
-		return this.memberService.signup();
+	public async signup(@Args('input') input: MemberInput): Promise<Member> {
+		try {
+			console.log('Mutation: sigup');
+			console.log('Input:', input);
+			return this.memberService.signup(input);
+		} catch (err) {
+			console.log('Error: ', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 	@Mutation(() => String)
 	@UsePipes(ValidationPipe)
 	public async login(@Args('input') input: LoginInput): Promise<string> {
-        console.log('Mutation: login');
-        console.log('Input:', input);
-		return this.memberService.login();
+		try {
+			console.log('Mutation: sigup');
+			console.log('Input:', input);
+			return this.memberService.login();
+		} catch (err) {
+			console.log('Error: ', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,13 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	public async signup(input: MemberInput): Promise<Member> {
+		//TODO: Hash password
 
-    constructor(@InjectModel("Member") private readonly memberModel:Model<null>){}
-	public async signup(): Promise<string> {
-		return 'signup executed!';
+		try {
+			const result = await this.memberModel.create(input);
+			
+			return result;
+		} catch (err) {
+			console.log('Error, Servise.model:', err);
+			throw new BadRequestException(err);
+		}
 	}
 
 	public async login(): Promise<string> {
