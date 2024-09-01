c6787b7dfbfb93139487e1e7c261f92737d34285
aris1377 <bek77519101@gmail.com>
2024-09-01 18:58:45 +0900

    fix: changed availablePropertySorts and availableAgentSorts

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,13 +1,13 @@
 import { ObjectId } from 'bson';
 
-export const availableAgentSorts = ['createAt', 'updateAt', 'memberLikes', 'memberViews', 'memberRank'];
-export const availableMembertSorts = ['createAt', 'updateAt', 'memberLikes', 'memberViews'];
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
+export const availableMembertSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
 
 export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availablePropertySorts = [
-	'createAt',
-	'updateAt',
+	'createdAt',
+	'updatedAt',
 	'propertyLikes',
 	'propertyViews',
 	'propertyRank',
