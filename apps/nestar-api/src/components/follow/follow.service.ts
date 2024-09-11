88f0b856011e841ee812c4d4a27ddbf01e66e927
aris1377 <bek77519101@gmail.com>
2024-09-11 15:34:00 +0900

    feat: integration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -8,7 +8,7 @@ import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { internalExecuteOperation } from '@apollo/server/dist/esm/ApolloServer';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -77,7 +77,10 @@ export class FollowService {
                                 { $skip: (page - 1) * limit },
                                 { $limit: limit },
                                 lookupAuthMemberLiked(memberId, "$followingId"),
-
+                                lookupAuthMemberFollowed({
+                                    followerId: memberId,
+                                    followingId: "$followingId"
+                                }),
                                 lookupFollowingData,
                                 { $unwind: '$followingData' },
                             ],
@@ -109,6 +112,10 @@ export class FollowService {
                                 { $skip: (page - 1) * limit },
                                 { $limit: limit },
                                   lookupAuthMemberLiked(memberId, "$followerId"),
+                                lookupAuthMemberFollowed({
+                                    followerId: memberId,
+                                    followingId: "$followerId"
+                                }),
                                 lookupFollowerData,
                                 { $unwind: '$followerData' },
                             ],
