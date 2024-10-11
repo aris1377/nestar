84829830c99a084bd582328daf14d3175475a69f
aris1377 <bek77519101@gmail.com>
2024-10-11 15:51:13 +0900

    fix: modifiy WebSocket Chatting Server final satge

--- apps/nestar-api/src/components/like/like.service.ts
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -1,37 +1,40 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
-import { Like, MeLiked } from '../../libs/dto/like/like';
-import { LikeInput } from '../../libs/dto/like/like.input';
-import { T } from '../../libs/types/common';
-import { Message } from '../../libs/enums/common.enum';
-import { OrdinaryInquiry } from '../../libs/dto/property/property.input';
-import { Properties } from '../../libs/dto/property/property';
-import { LikeGroup } from '../../libs/enums/like.enum';
-import { lookupFavorite } from '../../libs/config';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, ObjectId } from "mongoose";
+import { Like, MeLiked } from "../../libs/dto/like/like";
+import { LikeInput } from "../../libs/dto/like/like.input";
+import { T } from "../../libs/types/common";
+import { Message } from "../../libs/enums/common.enum";
+import { OrdinaryInquiry } from "../../libs/dto/property/property.input";
+import { Properties } from "../../libs/dto/property/property";
+import { LikeGroup } from "../../libs/enums/like.enum";
+import { lookupFavoriteData } from "../../libs/config";
 
 @Injectable()
 export class LikeService {
-    constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) { }
+	constructor(@InjectModel("Like") private readonly likeModel: Model<Like>) {}
 
-    public async toggleLike(input: LikeInput): Promise<number> {
-        const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
-        const exist = await this.likeModel.findOne(search).exec();
-        let modifier = 1;
-       if (exist) {
+	public async toggleLike(input: LikeInput): Promise<number> {
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
+		const exist = await this.likeModel.findOne(search).exec();
+		let modifier: number = 1;
+
+		if (exist) {
 			await this.likeModel.findOneAndDelete(search).exec();
-				modifier = -1;
-		} else  {
+			modifier = -1;
+		} else {
 			try {
 				await this.likeModel.create(input);
 			} catch (err) {
-				console.log('Error, Service.model:', err.message);
+				console.log("Error on toggleLike Service =>", err);
 				throw new BadRequestException(Message.CREATE_FAILED);
 			}
 		}
+
 		return modifier;
 	}
-    public async checkLikeExistence(input: LikeInput): Promise<MeLiked[]> {
+
+	public async checkLikeExistence(input: LikeInput): Promise<MeLiked[]> {
 		const { memberId, likeRefId } = input;
 		const result = await this.likeModel
 			.findOne({
@@ -47,36 +50,43 @@ export class LikeService {
 		const { page, limit } = input;
 		const match: T = { likeGroup: LikeGroup.PROPERTY, memberId: memberId };
 
-		const data: T = await this.likeModel.aggregate([
-		//likeloglarni beradi bizga bu qismi
-			{ $match: match },
-			{ $sort: { updatedAt: -1 } },
-			{
-				$lookup: {
-					from: 'properties',
-					localField: 'likeRefId',
-					foreignField: '_id',
-					as: 'favoriteProperty',
+		const data: T = await this.likeModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: { updatedAt: -1 } },
+				{
+					$lookup: {
+						from: "properties",
+						localField: "likeRefId",
+						foreignField: "_id",
+						as: "favoriteProperty",
+					},
 				},
-			},
-			{ $unwind: '$favoriteProperty' },
-			{
-				$facet: {
-					list: [
-						{ $skip: (page - 1) * limit },
-						{ $limit: limit },
-						lookupFavorite,
-						{ $unwind: '$favoriteProperty.memberData' },
-					],
-					metaCounter: [{ $count: 'total' }],
+				{
+					$unwind: "$favoriteProperty",
 				},
-			
-			},
-		]).exec(); 
-		const result: Properties = { list: [], metaCounter: data[0].metaCounter } 
-		result.list = data[0].list.map((ele)=>ele.favoriteProperty)
+				{
+					$facet: {
+						list: [
+							{ $skip: (page - 1) * limit },
+							{ $limit: limit },
+							lookupFavoriteData,
+							{ $unwind: "$favoriteProperty.memberData" },
+						],
+						metaCounter: [{ $count: "total" }],
+					},
+				},
+			])
+			.exec();
+
+		const result: Properties = {
+			list: [],
+			metaCounter: data[0].metaCounter,
+		};
+
+		result.list = data[0].list.map((ele: T) => ele.favoriteProperty);
+
+		// console.log("result on getFavoriteProperties =>", result);
 		return result;
 	}
 }
-
-
